c83402bbf174e94c708bf3682f85ea9b97ee0ca3
c.giacometti <camilla.giacometti@gmail.com>
2022-07-28 17:55:46 -0300

    versão final

--- linkedriven.sql
+++ linkedriven.sql
@@ -1,7 +1,6 @@
 /*QUESTÃO 1*/
-SELECT COUNT(experiences."endDate") AS currentExperiences 
-FROM experiences 
-GROUP BY "endDate" IS NOT NULL;
+SELECT COUNT ("endDate") AS currentExperiences 
+FROM experiences;
 
 /*QUESTÃO 2*/
 SELECT educations."userId" AS id, 
@@ -14,13 +13,14 @@ SELECT users.name AS writer ,
 COUNT(testimonials.id) AS testimonialsCount
 FROM testimonials
 JOIN users ON users.id = testimonials."writerId"
+WHERE testimonials."writerId" = 435
 GROUP BY users.name;
 
 /*QEUSTÃO 4*/
-SELECT MAX(jobs.salary) AS maximumSalary,
+SELECT MAX(jobs.salary) AS "maximumSalary",
 roles.name AS role
 FROM jobs
 JOIN roles ON roles.id = jobs."roleId"
 WHERE jobs.active = 'true'
 GROUP BY roles.name
-ORDER BY roles.name ASC;
+ORDER BY "maximumSalary" ASC;
